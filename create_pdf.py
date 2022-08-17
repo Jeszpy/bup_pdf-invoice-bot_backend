ae2d80c4c277f8a9a4c5ab84802e3cba0329cecc
Jezspy <hleb.lukashonak@gmail.com>
2022-08-17 17:58:28 +0300

    create post method

---
+++ create_pdf.py
@@ -0,0 +1,5 @@
+def take_pdf(organization: str,
+               requisites: str,
+               cards: int | None,
+               pouchs: int | None):
+    return 123
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import fastapi
-import fastapi.middleware.cors as cors
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+from datetime import datetime
+from create_pdf import take_pdf
 
+# uvicorn main:app --reload
 app = fastapi.FastAPI()
 
 app.add_middleware(
-    cors.CORSMiddleware,
+    CORSMiddleware,
     allow_origins=["*"],
     allow_methods=["*"],
     allow_credentials=True,
@@ -12,6 +17,20 @@ app.add_middleware(
 )
 
 
+class InvoiceData(BaseModel):
+    organization: str
+    requisites: str
+    cards: int | None
+    pouchs: int | None
+
+
 @app.get('/')
 def wakeup():
     return True
+
+
+@app.post('/create-invoice')
+def create_invoice(body: InvoiceData):
+    invoice_data = jsonable_encoder(body)
+    response = take_pdf(invoice_data['organization'], invoice_data['requisites'], invoice_data['cards'], invoice_data['pouchs'])
+    return response
